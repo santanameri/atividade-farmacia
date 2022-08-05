1e8a43242aaf0a41fa0055722af5efdc93e6c768
Merielly Santana <santana.meri10gmail.com>
2022-08-04 22:51:03 -0300

    Farmacia

---
+++ drogalera/src/main/java/com/generation/drogalera/DrogaleraApplication.java
@@ -0,0 +1,13 @@
+package com.generation.drogalera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogaleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrogaleraApplication.class, args);
+	}
+
+}
---
+++ drogalera/src/test/java/com/generation/drogalera/DrogaleraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.drogalera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogaleraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
